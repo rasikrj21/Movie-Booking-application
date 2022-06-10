10a2dbd2e2d11daa8d50f6d77cebbea4be2648dc
Rasik Umanath Thalkar <55530423+rasikrj21@users.noreply.github.com>
2022-06-10 20:49:26 +0530

    Checkpoint 2
    Created Project Environment and added the header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './common/Header/Header.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return(
+        <header>
+            <img className="siteLogo" src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" alt="Site Logo" />
+        </header> 
+    );   
+}
